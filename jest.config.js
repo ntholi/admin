3d0517999c93bdbcdc5b87c29bf0a2d0e0621b90
Ntholi Nkhatho <ntholi@live.com>
2023-12-27 18:37:30 +0200

    delete everything

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-  },
-};
--- src/core/Button/Button.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Button.css';
-
-type Props = {
-  children: React.ReactNode;
-};
-export default function Button({ children }: Props) {
-  return <button className='text-9xl'>{children}</button>;
-}
