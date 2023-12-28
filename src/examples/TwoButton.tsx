294f7a0cd1c11b462d07e354fb202a700c572362
Ntholi Nkhatho <ntholi@live.com>
2023-12-28 21:37:37 +0200

    Refactor TwoButton component to use a div instead of a button

--- src/examples/TwoButton.tsx
+++ src/examples/TwoButton.tsx
@@ -3,9 +3,9 @@ import { Button } from '@/components/ui/button';
 
 export default function TwoButton() {
   return (
-    <button className={'flex gap-3 p-5 border border-red-500'}>
+    <div className={'flex gap-3 p-5 border border-red-500'}>
       <Button>Button 1</Button>
       <Button variant={'ghost'}>Button 2</Button>
-    </button>
+    </div>
   );
 }
