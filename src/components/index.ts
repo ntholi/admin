d0bd709643f95803e53cc8c437424dca825b102c
Ntholi Nkhatho <ntholi@live.com>
2023-12-28 20:35:50 +0200

    inject css into final js file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
+import 'tailwindcss/tailwind.css';
+
 export { default as Button } from './Button';
 export { default as TwoButtons } from '../examples/TwoButton';
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import dts from 'vite-plugin-dts';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -28,6 +29,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    libInjectCss(),
     dts({
       rollupTypes: true,
     }),
