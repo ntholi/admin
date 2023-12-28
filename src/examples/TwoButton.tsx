66016623752137f3c5e3a09352777d52a937bfde
Ntholi Nkhatho <ntholi@live.com>
2023-12-28 18:39:59 +0200

    export TwoButtons

--- src/examples/TwoButton.tsx
+++ src/examples/TwoButton.tsx
@@ -3,9 +3,7 @@ import { Button } from '@/components';
 export default function TwoButton() {
   return (
     <button className={'flex gap-3 p-5 border border-red-500'}>
-      <Button variant='primary' size='md'>
-        One
-      </Button>
+      <Button>One</Button>
       <Button variant='secondary' size='md'>
         Two
       </Button>
